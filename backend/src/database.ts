7a4da05f94736298c4c08d0572098619fef9ae46
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2025-04-14 21:11:04 -0300

    conexão com o banco de dados

---
+++ backend/src/database.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'pg'
+import dotenv from 'dotenv';
+dotenv.config({ path: 'src/.env' });
+
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+} = process.env;
+
+const pool = new Pool({
+    host: DB_HOST,
+    port: Number(DB_PORT),
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
+});
+
+export default pool
