d9f5976ba10b39cde1573a78ceef089fbc82c6a9
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2025-04-16 18:38:56 -0300

    exemplos de rota

---
+++ backend/src/routes/customerRoutes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { CustomerController } from "../controllers/customerController";
+
+const customerRouter = Router();
+
+export default customerRouter;
---
+++ backend/src/routes/orderRoutes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { OrderController } from "../controllers/orderController";
+
+const orderRouter = Router();
+
+export default orderRouter;
