08dbe5d5d676c492fdf148181463bb369dc16db3
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2025-04-16 18:52:13 -0300

    rotas adicionadas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
-import express from 'express'
-import pool from './database'
+import express from 'express';
+import productRoutes from './routes/productRoutes';
+import customerRoutes from './routes/customerRoutes';
+import orderRoutes from './routes/orderRoutes';
 
-const app = express()
-const port = 3000
+const port = 3000;
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from TypeScript backend 👋')
-})
+app.use('/api/products', productRoutes);
+app.use('/api/customers', customerRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
